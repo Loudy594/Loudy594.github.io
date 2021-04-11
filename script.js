8515aa9c73e58945988a6b399a5e3c67cf806f2d
Loudy594 <62759558+Loudy594@users.noreply.github.com>
2021-04-11 15:36:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,10 +19,16 @@ countButton.onclick = function(){
   monker.innerHTML = "Wow. You're still here. Impressive!"
   } else if (count===6900) {
   monker.innerHTML = "Congrats on getting 6900. Why are you still playing this again?"
+  } else if (count ===10000) {
+  monker.innerHTML = "10000. Almost as high as my chess elo."
+  } else if (count === 50000) {
+  monker.innerHTML = "50000. Jesus. You are on the verge of becoming a monke no-life"
+  } else if (count === 69000) {
+  monker.innerHTML = "69000. hehe boi. you should really stop now. is your autoclicker wearing out?"
   } else if (count===100000) {
-  monker.innerHTML = "If you're still clicking, I honestly don't know what to say. Why are we still here... just to suffer"
+  monker.innerHTML = "If you're still clicking, I honestly don't know what to say. Why are we still here... just to suffer."
   } else if (count===1000000) {
-  monker.innerHTML = "what"
+  monker.innerHTML = "If you're not using an autoclicker you 100% have arthritis."
   } else if (count===1000000000) {
   monker.innerHTML = "bro... why are you still here? You've cilcked one billion times. Do you have nothing else to do with your life. You could be doing so much more with your life. You have serious issues."
   }
