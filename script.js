a46e5eeef43d7578c2beb753aa07a591ca7f210b
Loudy594 <62759558+Loudy594@users.noreply.github.com>
2021-04-12 15:01:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,19 @@ var displayCount = document.getElementById("displayCount");
 var goBack = document.getElementById("goBack");
 var count = 0;
 
-window.onbeforeunload = function(){
-  return 'Are you sure you want to leave?';
-};
+function goodbye(e) {
+    if(!e) e = window.event;
+    //e.cancelBubble is supported by IE - this will kill the bubbling process.
+    e.cancelBubble = true;
+    e.returnValue = 'You sure you want to leave?'; //This is displayed on the dialog
+
+    //e.stopPropagation works in Firefox.
+    if (e.stopPropagation) {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+}
+window.onbeforeunload=goodbye; 
 
 shopButton.onclick = function() {
     console.log("e");
