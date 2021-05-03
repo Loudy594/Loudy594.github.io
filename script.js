dd1c718234c62871d6e2f26929d1f86ef5605106
Loudy594 <62759558+Loudy594@users.noreply.github.com>
2021-05-03 14:54:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-window.onload = function() {
-    document.getElementById("result").innerHTML = "Monke button was pressed " + localStorage.clickcount + " times";
 
-    parseInt(localStorage.getItem('clickcount'))
-}
 
 function update() {
 
@@ -23,9 +19,6 @@ function clickCounter() {
         localStorage.clickcount = 1;
     }
     document.getElementById("result").innerHTML = "Monke button was pressed " + localStorage.clickcount + " times";
-
-
-    document.title = localStorage.clickcount + " Monkes"
 }
 
 function saveCount() {
@@ -39,56 +32,80 @@ function loadCount() {
 
 }
 
-function resetButton() {
-    parseInt(localStorage.getItem('clickcount'))
-
-    localStorage.clickcount = 0
-    cursors = 0;
-    cursorCost = 10;
-    document.getElementById('cursors').innerHTML = cursors;
-    document.getElementById('cursorCost').innerHTML = cursorCost;
-    document.getElementById("result").innerHTML = "Monke button was pressed " + localStorage.clickcount + " times";
-
-    monker.innerHTML = "Welcome To Monke Clicker created by charlie"
-}
 
 function shopButtonClick() {
 
 }
 
-var cursors = 0;
+    if (localStorage.cursors == null) {
+        var cursors = 0
+        localStorage.setItem('cursors', cursors)
+    }
 
+    if (localStorage.cursors != null) {
+        var cursors = localStorage.cursorCount
+    }
+
+    if (localStorage.cursorCost == null) {
+        var cursorCost = Math.floor(10 * Math.pow(1.1, parseInt(localStorage.cursors)));
+        console.log(cursorCost)
+
+        localStorage.setItem('cursorCost', cursorCost)
+    }
+
+    var cursorCost = parseInt(localStorage.cursorCost)
+    var cursorCount =  parseInt(localStorage.getItem('cursors'))
 
 function buyCursor() {
 
     //  document.getElementById('cursors').innerHTML = JSON.parse(localStorage.getItem('save')).cursors
 
-    var cursorCost = Math.floor(10 * Math.pow(1.1, cursors));
+    var cursorCost = Math.floor(10 * Math.pow(1.1, cursorCount));
 
     if (localStorage.clickcount < cursorCost) {
+
         alert('You do not have enough monkes to buy this!')
     } else if (localStorage.clickcount >= cursorCost) {
-        cursors = cursors + 1;
+        cursorCount = cursorCount + 1;
 
-
+        localStorage.setItem('cursors', cursorCount)
+        localStorage.setItem('cursorCost', cursorCost)
+        
         localStorage.clickcount = localStorage.clickcount - cursorCost;
-        document.getElementById('cursors').innerHTML = cursors;
+        document.getElementById('cursors').innerHTML = cursorCount;
         document.getElementById('result').innerHTML = "Monke button was pressed " + localStorage.clickcount + " times"
 
         window.setInterval(function() {
-
-            clickCounter(cursors);
+            clickCounter(cursorCount);
 
         }, 1000);
 
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1, cursors));
+    var nextCost = Math.floor(10 * Math.pow(1.1, cursorCount));
 
     document.getElementById('cursorCost').innerHTML = cursorCost;
 
 };
 
 
+window.onload = function() {
+    document.getElementById("result").innerHTML = "Monke button was pressed " + localStorage.clickcount + " times";
+
+    parseInt(localStorage.getItem('clickcount'))
+
+    document.getElementById('cursors').innerHTML = cursorCount
+    document.getElementById('cursorCost').innerHTML = cursorCost
+
+    for (var i=0; i < cursorCount; i++) {
+        window.setInterval(function() {
+            clickCounter(cursorCount);
+
+        }, 1000);
+    }
+
+}
+
 window.onbeforeunload = function() {
     return "Are you sure you want to leave the worlds best game?";
+    console.log('e')
 };
